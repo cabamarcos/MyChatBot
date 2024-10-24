d94ee9754e43cfadd602985b3a3dc9536d7afaf7
cabamarcos <marcos.caballero.0203@gmail.com>
2024-10-24 14:09:09 +0200

    Cambio a llama

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ const $loading = $('.loading')
 let messages = []
 let end = false
 
-const SELECTED_MODEL = 'gemma-2b-it-q4f32_1-MLC'
+const SELECTED_MODEL = 'Llama-3-8B-Instruct-q4f32_1-MLC-1k'
 
 const engine = await CreateWebWorkerMLCEngine(
     new Worker('./worker.js', { type: 'module' }),
